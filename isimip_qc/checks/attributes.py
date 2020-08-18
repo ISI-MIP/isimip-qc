a2ab727de07ebbd459dd87e20992690892a276d0
Matthias Buechner <buechner@pik-potsdam.de>
2020-08-18 12:23:35 +0200

    add boolean flags for warnings and errors in checks

--- isimip_qc/checks/attributes.py
+++ isimip_qc/checks/attributes.py
@@ -5,7 +5,7 @@ def check_institution(file):
     institution = file.dataset.getncattr('institution')
     if institution is None:
         file.warn('institution is missing.')
-        file.clean = False
+        file.has_warnings = True
 
 
 def check_contact(file):
@@ -14,7 +14,7 @@ def check_contact(file):
         name, address = parseaddr(contact)
         if not address:
             file.warn('contact="%s" is not a proper address.', contact)
-            file.clean = False
+            file.has_warnings = True
     else:
         file.warn('contact is missing.')
-        file.clean = False
+        file.has_warnings = True
