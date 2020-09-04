9e0106fd13d1a93822cbd0768d50de84003603fb
Matthias Buechner <m.buechner@gmail.com>
2020-09-04 15:46:19 +0200

    avoid removing of renamed attibutes

--- isimip_qc/checks/dataset.py
+++ isimip_qc/checks/dataset.py
@@ -44,14 +44,14 @@ def check_lower_case(file):
 
         for attr in variable.__dict__:
             if attr not in ['_FillValue']:
-                if not attr.islower():
-                    file.warn('Attribute "%s.%s" is not lower case.', variable_name, attr, fix={
-                        'func': fix_rename_attr,
-                        'args': (file, variable_name, attr)
-                    })
                 if attr not in ['axis', 'standard_name', 'long_name', 'calendar', 'missing_value', 'units']:
                     file.warn('Attribute "%s.%s" is not needed.', variable_name, attr, fix={
                         'func': fix_remove_attr,
+                        'args': (file, variable_name, attr)
+                    })
+                elif not attr.islower():
+                    file.warn('Attribute "%s.%s" is not lower case.', variable_name, attr, fix={
+                        'func': fix_rename_attr,
                         'args': (file, variable_name, attr)
                     })
 
