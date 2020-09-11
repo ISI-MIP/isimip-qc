e03aaff531abc62ce493764f7d3ae01542b34bd4
Matthias Buechner <m.buechner@gmail.com>
2020-09-11 12:23:47 +0200

    early detect 2d or 3d data

--- isimip_qc/checks/dimensions.py
+++ isimip_qc/checks/dimensions.py
@@ -1,10 +1,19 @@
 from ..config import settings
 
+def check_1_dimensions(file):
+    variable_name = file.specifiers.get('variable')
+    variable = file.dataset.variables.get(variable_name)
+    dim_len = len(variable.dimensions)
+
+    # detect 2d or 3d data
+    if dim_len == 3:
+        file.is_2d = True
+    elif dim_len == 4:
+        file.is_3d = True
 
 def check_lon_dimension(file):
     if file.dataset.dimensions.get('lon') is None:
         file.error('Dimension \'lon\' is missing.')
-    print('1')
 
 def check_lat_dimension(file):
     if file.dataset.dimensions.get('lat') is None:
@@ -16,21 +25,18 @@ def check_time_dimension(file):
         file.error('Dimension \'time\' is missing.')
 
 
-def check_dimensions(file):
-    print('2')
+def check_2_dimensions(file):
     # check dimension order
     variable_name = file.specifiers.get('variable')
     variable = file.dataset.variables.get(variable_name)
 
     dim_len = len(variable.dimensions)
-    if dim_len == 3:
-        file.is_2d = True
+    if file.is_2d:
         if variable.dimensions[0] != 'time' or variable.dimensions[1] != 'lat' or variable.dimensions[2] != 'lon':
             file.warn('%s dimension order %s should be ["time", "lat", "lon"].', variable_name, variable.dimensions)
         else:
             file.info('Dimensions for variable "%s" look good: %s', variable_name, variable.dimensions)
-    elif dim_len == 4:
-        file.is_3d = True
+    elif file.is_3d:
         if variable.dimensions[0] != 'time' or variable.dimensions[1] not in ['depth'] or variable.dimensions[2] != 'lat' or variable.dimensions[2] != 'lon':
             file.warn('%s dimension order %s should be ["time", "depth" , "lat", "lon"].', variable_name, variable.dimensions)
         else:
