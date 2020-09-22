86f21f0786ee0cabf7276a3baf7e5375651782dc
Matthias Buechner <m.buechner@gmail.com>
2020-09-22 14:38:41 +0200

    check for proper grid size

--- isimip_qc/checks/dimensions.py
+++ isimip_qc/checks/dimensions.py
@@ -2,13 +2,25 @@ from ..config import settings
 
 
 def check_lon_dimension(file):
+    lon_definition = settings.DEFINITIONS['dimensions'].get('lon')
     if file.dataset.dimensions.get('lon') is None:
-        file.error('Dimension "lon" is missing.')
+        file.error('Longitude dimension "lon" is missing.')
+    else:
+        if lon_definition['size'] != file.dataset.dimensions.get('lon').size:
+            file.warn('Less than %s longitudes found (%s).', lon_definition['size'], file.dataset.dimensions.get('lon').size)
+        else:
+            file.info('%s longitudes defined.', lon_definition['size'])
 
 
 def check_lat_dimension(file):
+    lat_definition = settings.DEFINITIONS['dimensions'].get('lat')
     if file.dataset.dimensions.get('lat') is None:
-        file.error('Dimension "lat" is missing.')
+        file.error('Latitude dimension "lat" is missing.')
+    else:
+        if lat_definition['size'] != file.dataset.dimensions.get('lat').size:
+            file.warn('Less than %s latitudes found (%s).', lat_definition['size'], file.dataset.dimensions.get('lat').size)
+        else:
+            file.info('%s latitudes defined.', lat_definition['size'])
 
 
 def check_time_dimension(file):
@@ -31,12 +43,12 @@ def check_dimensions(file):
         if variable.dimensions[0] != 'time' or variable.dimensions[1] != 'lat' or variable.dimensions[2] != 'lon':
             file.error('%s dimension order %s should be ["time", "lat", "lon"].', file.variable_name, variable.dimensions)
         else:
-            file.info('Dimensions for variable "%s" look good: %s', file.variable_name, variable.dimensions)
+            file.info('Dimensions for variable "%s" look good: %s.', file.variable_name, variable.dimensions)
     elif file.is_3d:
         if variable.dimensions[0] != 'time' or file.dim_vertical not in ['depth'] or variable.dimensions[2] != 'lat' or variable.dimensions[3] != 'lon':
             file.error('Found %s dimension dependencies and order %s. Should be ["time", "depth" , "lat", "lon"].', file.variable_name, variable.dimensions)
         else:
-            file.info('Dimensions for variable "%s" look good', file.variable_name)
+            file.info('Dimensions for variable "%s" look good.', file.variable_name)
     else:
         file.error('Variable "%s" neither holds 2d or 3d data. (dim=%s)', dim_len)
 
