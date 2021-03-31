f7a22d03c417b2fb6c77374cec547ab932caf1a5
Matthias Buechner <m.buechner@gmail.com>
2021-03-31 13:15:18 +0200

    check for lake levels order to be positive down

--- isimip_qc/checks/variables/var3d.py
+++ isimip_qc/checks/variables/var3d.py
@@ -56,6 +56,15 @@ def check_3d_variable(file):
 
             # for lakes sector
             if file.dim_vertical == 'levlak':
+                levlak_first = file.dataset.variables.get(file.dim_vertical)[0]
+                levlak_last = file.dataset.variables.get(file.dim_vertical)[-1]
+
+                if levlak_first > levlak_last:
+                    file.warn('"levlak" in wrong order. Should increase with depth . (found %s to %s)', levlak_first, levlak_last)
+                else:
+                    file.info('"levlak" order looks good (positive down).')
+
+
                 depth_file = file.dataset.variables.get('depth')
 
                 if depth_file is None:
