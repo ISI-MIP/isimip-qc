bde2ccaac3ef3a0dbbe3853191dbcf476e99d20b
Jochen Klar <mail@jochenklar.de>
2023-04-13 17:40:09 +0200

    Fix get_experiments

--- isimip_qc/utils/experiments.py
+++ isimip_qc/utils/experiments.py
@@ -6,14 +6,17 @@ def get_experiment(specifiers):
     soc_scenario = specifiers.get('soc_scenario')
     sens_scenario = specifiers.get('sens_scenario')
 
-    for specifier, experiment in settings.DEFINITIONS.get('experiments').items():
-        for period in settings.DEFINITIONS.get('period', []):
-            experiment_period = experiment.get(period, {})
+    for experiment_specifier, experiment_values in settings.DEFINITIONS.get('experiments').items():
+        for period_specifier, period_values in settings.DEFINITIONS.get('period', []).items():
+            if (period_values.get('start_year') <= specifiers.get('start_year') and
+                    period_values.get('end_year') >= specifiers.get('end_year')):
 
-            if (isinstance(experiment_period, dict) and
-                    experiment_period.get('climate') == climate_scenario and
-                    experiment_period.get('soc') == soc_scenario and
-                    (sens_scenario == 'default' or
-                        experiment_period.get('climate_sens') == sens_scenario or
-                        experiment_period.get('soc_sens') == sens_scenario)):
-                return specifier
+                experiment_period = experiment_values.get(period_specifier, {})
+
+                if (isinstance(experiment_period, dict) and
+                        experiment_period.get('climate') == climate_scenario and
+                        experiment_period.get('soc') == soc_scenario and
+                        (sens_scenario == 'default' or
+                            experiment_period.get('climate_sens') == sens_scenario or
+                            experiment_period.get('soc_sens') == sens_scenario)):
+                    return experiment_specifier
