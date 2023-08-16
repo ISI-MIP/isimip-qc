8d1a3147ceec9dad20b1f3063b4b5f24da8749c5
Jochen Klar <mail@jochenklar.de>
2023-08-16 15:05:57 +0200

    Improve check_experiments

--- isimip_qc/checks/experiments.py
+++ isimip_qc/checks/experiments.py
@@ -2,11 +2,15 @@ from ..utils.experiments import get_experiment
 
 
 def check_experiment(file):
-    if file.specifiers.get('time_step') == 'fixed':
+    if file.specifiers.get("time_step") == "fixed":
         return
 
     experiment = get_experiment(file.specifiers)
     if experiment is None:
-        file.critical('No valid experiment found for {climate_scenario}, {soc_scenario} and {sens_scenario}'.format(**file.specifiers))
+        file.critical(
+            "No valid experiment found for {climate_scenario}, {soc_scenario}, {sens_scenario} between {start_year} and {end_year}.".format(
+                **file.specifiers
+            )
+        )
     else:
-        file.info('Experiment looks good (%s).', experiment)
+        file.info("Experiment looks good (%s).", experiment)
