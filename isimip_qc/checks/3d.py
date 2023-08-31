8e18e0fe114eede5df76e122b915d2be63d8797c
Matthias Buechner <m.buechner@gmail.com>
2023-08-31 14:59:52 +0200

    check for correct number of dimensions

--- isimip_qc/checks/3d.py
+++ isimip_qc/checks/3d.py
@@ -1,3 +1,5 @@
+from isimip_qc.config import settings
+
 def check_3d(file):
     crop = file.specifiers.get('crop')
     irrigation = file.specifiers.get('irrigation')
@@ -30,22 +32,35 @@ def check_3d(file):
         file.critical('Variable "%s" from file name not found inside the file! Check NetCDF header. Stopping tool!', file.variable_name)
         raise SystemExit(1)
 
-    dim_len = len(variable.dimensions)
+    definition = settings.DEFINITIONS.get('variable', {}).get(file.specifiers.get('variable'))
+
+    # check for number of variable dependencies
+
+    file_dim_len = len(variable.dimensions)
+
+    if 'dimensions' in definition:
+        settings_dim_len = len(definition['dimensions'])
+    else:
+        settings_dim_len = 3
+
+    if file_dim_len != settings_dim_len:
+        file.critical('Dimension missing (%s found, %s expected). Declare as %s%s. ',
+                      file_dim_len, settings_dim_len, file.variable_name, str(definition['dimensions']).replace('\'',''))
 
     # detect 2d or 3d data
     file.is_time_fixed = False
 
     if file.specifiers.get('time_step') == 'fixed':
         file.is_time_fixed = True
-        if dim_len > 2:
+        if file_dim_len > 2:
             file.critical('File has fixed data but more than 2 dimensions. Remove "time" dimension if present.')
             return
-    elif dim_len == 3:
+    elif file_dim_len == 3:
         file.is_2d = True
-    elif dim_len == 4:
+    elif file_dim_len == 4:
         file.is_3d = True
         if variable.dimensions[1] in ['time', 'lat', 'lon']:
-            for pos in range(0, dim_len):
+            for pos in range(0, file_dim_len):
                 if variable.dimensions[pos] not in ['time', 'lat', 'lon']:
                     break
             file.critical('Variable "%s" has "time", "lat" or "lon" as second dependeny. Depencency order must be [time, %s, lat, lon]. "time" is first always.', file.variable_name, variable.dimensions[pos])
