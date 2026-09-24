c088183846171f67f82d343170e41a098facbdf0
Matthias Büchner <m.buechner@gmail.com>
2026-09-24 11:12:17 +0200

    allow semicolon to separate multiple contacts

--- isimip_qc/utils/contact.py
+++ isimip_qc/utils/contact.py
@@ -3,7 +3,7 @@ import re
 candidate_pattern = r'\S+@\S+'
 addr_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
 name_pattern = fr'[^<>,\s][^<>,]*\s<{addr_pattern}>'
-full_pattern = fr'^{name_pattern}(,\s{name_pattern})*$'
+full_pattern = fr'^{name_pattern}([,;]\s{name_pattern})*$'
 
 
 def match_addrs(contact):
